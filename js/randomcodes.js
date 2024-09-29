07ff36f2f44c9e231400bfc280df43a994a5a41c
Colleen Ponraj <colleenponraj@Colleens-MacBook-Pro.local>
2024-09-29 11:08:20 -0700

    added javascript to create random characters

---
+++ js/randomcodes.js
@@ -0,0 +1,25 @@
+/*------Random Codes------*/
+
+//Function to generate combination of characters
+function generateCode(){
+    var code = ' ';//initialize to null value
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    //Generate character multiple times using a loop
+    for (i=1; i<= 8; i++){
+        var char = Math.random()*str.length;
+        code += str.charAt(char)
+    }
+    return code;
+}
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+//Disable Button
+
+function disableButton(){
+    document.getElementById("submit").disabled = true;
+}
+
+//Activate function
+disableButton();
